1b96ef183ef84449464001f9cbe645e5e6da8a00
bernsrepublic <bernsrepublic@gmail.com>
2023-02-27 15:44:43 -0800

    background attempt to resolve

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 //alert(test);
+function tile(url, left, bottom, width, height) {
+  for (let h = 0; h < height; h++) {
+    for (let w = 0; w < width; w++) {
+      newImage(url, left + w * 100, bottom + h * 100);
+    }
+  }
+}
 const newImage = (src, left, bottom) => {
   let object = document.createElement("img");
   object.src = src;
@@ -14,6 +21,14 @@ const newItem = (src, left, bottom) => {
     object.remove();
   });
 };
+
+let horizon = window.innerHeight / 1.75;
+let heightOfSky = window.innerHeight - horizon;
+let heightOfGrass = horizon;
+
+tile("assets/sky.png", 0, horizon, window.innerWidth / 100, heightOfSky / 100);
+tile("assets/grass.png", 0, 0, window.innerWidth / 100, heightOfGrass / 100);
+
 newImage("../assets/green-character.gif", 100, 100);
 newImage("../assets/pine-tree.png", 450, 200);
 newImage("../assets/tree.png", 200, 300);
