663867fa6a70594b45157acf8f14674619e63785
bernsrepublic <bernsrepublic@gmail.com>
2023-02-27 15:48:01 -0800

    corrected image relevant path

--- index.js
+++ index.js
@@ -29,16 +29,16 @@ let heightOfGrass = horizon;
 tile("assets/sky.png", 0, horizon, window.innerWidth / 100, heightOfSky / 100);
 tile("assets/grass.png", 0, 0, window.innerWidth / 100, heightOfGrass / 100);
 
-newImage("../assets/green-character.gif", 100, 100);
-newImage("../assets/pine-tree.png", 450, 200);
-newImage("../assets/tree.png", 200, 300);
-newImage("../assets/pillar.png", 350, 100);
-newImage("../assets/crate.png", 150, 200);
+newImage("assets/green-character.gif", 100, 100);
+newImage("assets/pine-tree.png", 450, 200);
+newImage("assets/tree.png", 200, 300);
+newImage("assets/pillar.png", 350, 100);
+newImage("assets/crate.png", 150, 200);
 newImage("../assets/well.png", 500, 425);
 
-newItem("../assets/sword.png", 500, 405);
-newItem("../assets/sheild.png", 165, 185);
-newItem("../assets/staff.png", 600, 100);
+newItem("assets/sword.png", 500, 405);
+newItem("assets/sheild.png", 165, 185);
+newItem("assets/staff.png", 600, 100);
 //****************************************************************** */
 // function tile(src, left, bottom, width, height) {
 //   for (let h = 0; h < height; h++) {
