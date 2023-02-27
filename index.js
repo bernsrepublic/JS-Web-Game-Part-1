359b01a4f83d1c759ab539fdb3c71bb0f5d4c583
bernsrepublic <bernsrepublic@gmail.com>
2023-02-27 15:37:03 -0800

    re-arranged the code blocks for the functions and invoking of functions

--- index.js
+++ index.js
@@ -1,51 +1,49 @@
 //alert(test);
-const newImage = (src, position, left, bottom) => {
+const newImage = (src, left, bottom) => {
   let object = document.createElement("img");
   object.src = src;
-  object.style.position = position;
+  object.style.position = "fixed";
   object.style.left = left + "px";
   object.style.bottom = bottom + "px";
   document.body.append(object);
   return object;
 };
-
-newImage("../assets/green-character.gif", "fixed", 100, 100);
-newImage("../assets/pine-tree.png", "fixed", 450, 200);
-newImage("../assets/tree.png", "fixed", 200, 300);
-newImage("../assets/pillar.png", "fixed", 350, 100);
-newImage("../assets/crate.png", "fixed", 150, 200);
-newImage("../assets/well.png", "fixed", 500, 425);
-
-const newItem = (src, position, left, bottom) => {
-  let object = newImage(src, position, left, bottom);
+const newItem = (src, left, bottom) => {
+  let object = newImage(src, left, bottom);
   object.addEventListener("dblc'lick", () => {
     object.remove();
   });
 };
+newImage("../assets/green-character.gif", 100, 100);
+newImage("../assets/pine-tree.png", 450, 200);
+newImage("../assets/tree.png", 200, 300);
+newImage("../assets/pillar.png", 350, 100);
+newImage("../assets/crate.png", 150, 200);
+newImage("../assets/well.png", 500, 425);
 
-newItem("../assets/sword.png", "fixed", 500, 405);
-newItem("../assets/sheild.png", "fixed", 165, 185);
-newItem("../assets/staff.png", "fixed", 600, 100);
+newItem("../assets/sword.png", 500, 405);
+newItem("../assets/sheild.png", 165, 185);
+newItem("../assets/staff.png", 600, 100);
 //****************************************************************** */
-function tile(src, left, bottom, width, height) {
-  for (let h = 0; h < height; h++) {
-    for (let w = 0; w < width; w++) {
-      newImage(src, left + w * 100, bottom + h * 100);
-    }
-  }
-}
-let horizon = window.innerHeight / 1.75;
-let heightOfSky = window.innerHeight - horizon;
-let heightOfGrass = horizon;
+// function tile(src, left, bottom, width, height) {
+//   for (let h = 0; h < height; h++) {
+//     for (let w = 0; w < width; w++) {
+//       newImage(src, left + w * 100, bottom + h * 100);
+//     }
+//   }
+// }
+// let horizon = window.innerHeight / 1.75;
+// let heightOfSky = window.innerHeight - horizon;
+// let heightOfGrass = horizon;
 
-tile(
-  "../assets/sky.png",
-  0,
-  horizon,
-  window.innerWidth / 105,
-  heightOfSky / 100
-);
-tile("../assets/grass.png", 0, 0, window.innerWidth / 100, heightOfGrass / 100);
+// tile(
+//   "../assets/sky.png",
+//   0,
+//   horizon,
+//   window.innerWidth / 105,
+//   heightOfSky / 100
+// );
+// tile("../assets/grass.png", 0, 0, window.innerWidth / 100, heightOfGrass / 100);
 // ****************************************************************************
 // let sword = document.createElement("img");
 // sword.src = "assets/sword.png";
