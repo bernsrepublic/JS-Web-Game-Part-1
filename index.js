1ac21c1bd03017cb088c389637feb0536ba8fa9d
bernsrepublic <bernsrepublic@gmail.com>
2023-02-27 16:41:05 -0800

    final commit

--- index.js
+++ index.js
@@ -5,10 +5,10 @@
 // - added the "px" property-value in the function
 // - fixed the relevant path of the image. somehow using "../" doesn't work.
 
-function tile(url, left, bottom, width, height) {
+function tile(src, left, bottom, width, height) {
   for (let h = 0; h < height; h++) {
     for (let w = 0; w < width; w++) {
-      newImage(url, left + w * 100, bottom + h * 100);
+      newImage(src, left + w * 100, bottom + h * 100);
     }
   }
 }
@@ -32,19 +32,25 @@ let horizon = window.innerHeight / 1.75;
 let heightOfSky = window.innerHeight - horizon;
 let heightOfGrass = horizon;
 
-tile("assets/sky.png", 0, horizon, window.innerWidth / 100, heightOfSky / 100);
-tile("assets/grass.png", 0, 0, window.innerWidth / 100, heightOfGrass / 100);
+tile(
+  "../assets/sky.png",
+  0,
+  horizon,
+  window.innerWidth / 100,
+  heightOfSky / 100
+);
+tile("../assets/grass.png", 0, 0, window.innerWidth / 100, heightOfGrass / 100);
 
-newImage("assets/green-character.gif", 100, 100);
-newImage("assets/tree.png", 200, 300);
-newImage("assets/pillar.png", 350, 100);
-newImage("assets/pine-tree.png", 450, 200);
-newImage("assets/crate.png", 150, 200);
-newImage("assets/well.png", 500, 425);
+newImage("../assets/green-character.gif", 100, 100);
+newImage("../assets/tree.png", 200, 300);
+newImage("../assets/pillar.png", 350, 100);
+newImage("../assets/pine-tree.png", 450, 200);
+newImage("../assets/crate.png", 150, 200);
+newImage("../assets/well.png", 500, 425);
 
-newItem("assets/sword.png", 500, 405);
-newItem("assets/sheild.png", 165, 185);
-newItem("assets/staff.png", 600, 100);
+newItem("../assets/sword.png", 500, 405);
+newItem("../assets/sheild.png", 165, 185);
+newItem("../assets/staff.png", 600, 100);
 //****************************************************************** */
 // function tile(src, left, bottom, width, height) {
 //   for (let h = 0; h < height; h++) {
