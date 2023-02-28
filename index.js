26558f1601aed946edcc2b2ca1ba04fd8c5d340a
bernsrepublic <bernsrepublic@gmail.com>
2023-02-27 17:41:58 -0800

    "added event listener"

--- index.js
+++ index.js
@@ -38,15 +38,33 @@ let heightOfGrass = horizon;
 tile("assets/sky.png", 0, horizon, window.innerWidth / 100, heightOfSky / 100);
 tile("assets/grass.png", 0, 0, window.innerWidth / 100, heightOfGrass / 100);
 
-newImage("assets/green-character.gif", 100, 100);
-newImage("assets/tree.png", 200, 300);
-newImage("assets/pillar.png", 350, 100);
-newImage("assets/pine-tree.png", 450, 200);
-newImage("assets/crate.png", 150, 200);
-newImage("assets/well.png", 500, 425);
-newItem("assets/sword.png", 500, 405);
-newItem("assets/sheild.png", 165, 185);
-newItem("assets/staff.png", 600, 100);
+function newInventory() {
+  let inventory = document.createElement("div");
+  inventory.style.position = "fixed";
+  inventory.style.bottom = "0px";
+  inventory.style.left = "0px";
+  inventory.style.width = "100%";
+  inventory.style.height = "100px";
+  inventory.style.display = "flex";
+  inventory.style.flexDirection = "row";
+  inventory.style.alignItems = "center";
+  inventory.style.justifyContent = "space-evenly";
+  inventory.style.border = "2px solid black";
+  inventory.style.backgroundColor = "brown";
+  document.body.append(inventory);
+}
+
+newInventory();
+newImage("assets/green-character.gif", 100, 250);
+newImage("assets/tree.png", 200, 450);
+newImage("assets/pillar.png", 350, 250);
+newImage("assets/pine-tree.png", 450, 350);
+newImage("assets/crate.png", 150, 350);
+newImage("assets/well.png", 500, 575);
+
+newItem("assets/sword.png", 500, 555);
+newItem("assets/sheild.png", 165, 335);
+newItem("assets/staff.png", 600, 250);
 
 //****************************************************************** */
 // function tile(src, left, bottom, width, height) {
